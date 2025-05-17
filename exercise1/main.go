86e12748d14f95f9c8db76dd2fd9f768658f3b5b
Felipe Fuerback <felipe.fuerback@authorhealth.com>
2025-05-17 16:46:33 -0300

    Initial commit

---
+++ exercise1/main.go
@@ -0,0 +1,16 @@
+package main
+
+func main() {
+	data := [][]int{
+		{1, 2, 3},
+		{4, 5},
+		{},
+		{7, 8, 9},
+	}
+
+	sumColValues(data)
+}
+
+func sumColValues(data [][]int) []int {
+	return []int{}
+}
---
+++ exercise2/main.go
@@ -0,0 +1,10 @@
+package main
+
+func main() {
+	list := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	binarySearch(5, list)
+}
+
+func binarySearch(target int, list []int) bool {
+	return false
+}
